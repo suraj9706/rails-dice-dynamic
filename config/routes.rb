e29fa6f83017c54fa850a7e0b0807a44a537b0c5
suraj9706 <136658220+suraj9706@users.noreply.github.com>
2023-07-25 04:35:10 +0000

    test 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 # config/routes.rb
 Rails.application.routes.draw do
+  root 'dice#roll'  # Set the root path to the roll action in the DiceController
+
   get '/roll_dice/:num_of_dice/:sides', to: 'dice#roll'
 end
