9a924410303ab9f595636db380ee4075c5b98f28
suraj9706 <136658220+suraj9706@users.noreply.github.com>
2023-07-25 05:00:12 +0000

    final

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -12,10 +12,18 @@ class DiceController < ApplicationController
     # Render the appropriate view template based on the number of dice
     if @num_of_dice == 1
       render 'dice_roll'
+    elsif @num_of_dice == 2 && @sides == 6
+      render 'dice_rolls_2' # Use this template for 2d6
     elsif @num_of_dice == 5
       render 'dice_rolls_5'
+    elsif @num_of_dice == 50
+      render 'dice_rolls_50'
     else
       render 'dice_rolls'
     end
+  end
+
+  def index
+    # This action will render the index.html.erb template for the root URL
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  # Your other routes might be defined here
+  # Define the root URL route
+  root 'dice#index'
 
+  # Define the route for rolling dice
   get '/dice/:num_of_dice/:sides', to: 'dice#roll'
 end
