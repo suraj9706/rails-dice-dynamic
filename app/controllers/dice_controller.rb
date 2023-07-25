f352055d5f503a8cbf29945707d55627e1f57d44
suraj9706 <136658220+suraj9706@users.noreply.github.com>
2023-07-25 04:47:53 +0000

    test 3

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,7 +1,5 @@
 # app/controllers/dice_controller.rb
 class DiceController < ApplicationController
-  skip_forgery_protection
-
   def roll
     @num_of_dice = params[:num_of_dice].to_i
     @sides = params[:sides].to_i
@@ -9,6 +7,15 @@ class DiceController < ApplicationController
 
     @num_of_dice.times do
       @rolls << rand(1..@sides)
+    end
+
+    # Render the appropriate view template based on the number of dice
+    if @num_of_dice == 1
+      render 'dice_roll'
+    elsif @num_of_dice == 5
+      render 'dice_rolls_5'
+    else
+      render 'dice_rolls'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  root 'dice#roll'  # Set the root path to the roll action in the DiceController
+  # Your other routes might be defined here
 
-  get '/roll_dice/:num_of_dice/:sides', to: 'dice#roll'
+  get '/dice/:num_of_dice/:sides', to: 'dice#roll'
 end
